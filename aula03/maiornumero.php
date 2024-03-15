8ed6962d5e0ff5a2f464ffe777f0f55ed04aac66
Paulo Prates <paulo.prates01@hotmail.com>
2024-03-14 21:57:56 -0300

    Aula 03: While, Do While, For

---
+++ aula03/maiornumero.php
@@ -0,0 +1,12 @@
+<?php
+    $numeros = array(0, 1, 2, 3, 4, 5, 7, 10);
+    $maior = $numeros[0];
+
+    foreach($numeros as $numero) {
+        if($numero > $maior) {
+            $maior = $numero;
+        }
+    }
+
+    echo "Maior Número: " . $maior;
+?>
