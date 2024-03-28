ff2a16cb8f98943d5c7966ab34f3cadf747c32b1
Paulo Prates <paulo.prates01@hotmail.com>
2024-03-28 20:22:46 -0300

    exercicios de funções

---
+++ aula04/exercicio1.php
@@ -0,0 +1,8 @@
+<?php
+    function somarNumeros($num1, $num2) {
+        $soma = $num1 + $num2;
+        echo "$num1 + $num2 = $soma";
+    }
+
+    somarNumeros(5, 4);
+?>
---
+++ aula04/function.php
@@ -0,0 +1,15 @@
+<?php
+    function anderson() {
+        echo "Olá Paulo! Narcisista!";
+    }
+
+    anderson();
+
+    function desvioDeSepto($fname) {
+        echo "$fname sobrenome. <br>";
+    }
+
+    desvioDeSepto("João");
+    desvioDeSepto("José");
+    desvioDeSepto("Jailson");
+?>
