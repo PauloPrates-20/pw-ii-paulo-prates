30d56e856daea3c9612bcc26711586d29b9dc89a
Paulo Prates <paulo.prates01@hotmail.com>
2024-03-28 20:38:36 -0300

    aula 04

--- aula04/exercicio1.php
+++ aula04/exercicio1.php
@@ -5,4 +5,18 @@
     }
 
     somarNumeros(5, 4);
+    echo "<br>";
+
+    function sumirNumeros($num1, $num2) {
+        $nums = array();
+        for($i = 0; $i < 100; $i++) {
+            array_push($nums, $i);
+        }
+        foreach($nums as $num) {
+            if($num != $num1 && $num != $num2) {
+            echo "$num, ";
+            }
+        }
+    }
+    sumirNumeros(5, 10);
 ?>
